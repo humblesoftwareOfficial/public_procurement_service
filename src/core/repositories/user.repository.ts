da20bff0b34bc2ecd5a56e009c322a2d90164ae4
humbleDev <lamine.thiam@ldb.sn>
2024-04-08 18:59:17 +0000

    small fix

--- src/core/repositories/user.repository.ts
+++ src/core/repositories/user.repository.ts
@@ -5,7 +5,7 @@ export class UserRepository<T>
   extends MongoGenericRepository<T>
   implements IUserRepository<T> {
   findByEmail(email: string): Promise<T> {
-    return this._repository.findOne({ email }).exec()
+    return this._repository.findOne({ email: email }).exec()
   }
     
   }
