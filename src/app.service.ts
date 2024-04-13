c7ddda70a011b1eedb20cf59e34b71f60f56780c
humbleDev <lamine.thiam@ldb.sn>
2024-04-13 18:40:21 +0000

    update recap data

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Result, succeed } from './config/http-response';
 import { IGenericDataServices } from './core/generics/generic-data.services';
 import { ENoticeAwardType } from './features/provisional-notice-award/provisional-notice-award.helper';
+import { EProcurementType } from './features/procurement-plan/procurement-plan.helper';
 
 @Injectable()
 export class AppService {
@@ -12,11 +13,42 @@ export class AppService {
 
   async getRecapData(): Promise<Result> {
     try {
-      const [procurement_plans, provisional_notice_awards, definitive_notice_awards, general_notices] = await Promise.all([
+      const [
+        procurement_plans,
+        provisional_notice_awards,
+        definitive_notice_awards,
+        general_notices,
+        procurement_plans_fournitures,
+        procurement_plans_travaux,
+        procurement_plans_prestations,
+        procurement_plans_services,
+      ] = await Promise.all([
         this.dataServices.procurement_plans.count({ isDeleted: false }),
-        this.dataServices.provisional_notice_award.count({ isDeleted: false, type: ENoticeAwardType.PROVISIONAL }),
-        this.dataServices.provisional_notice_award.count({ isDeleted: false, type: ENoticeAwardType.DEFINITIVE }),
+        this.dataServices.provisional_notice_award.count({
+          isDeleted: false,
+          type: ENoticeAwardType.PROVISIONAL,
+        }),
+        this.dataServices.provisional_notice_award.count({
+          isDeleted: false,
+          type: ENoticeAwardType.DEFINITIVE,
+        }),
         this.dataServices.general_notice.count({ isDeleted: false }),
+        this.dataServices.procurement_plans.count({
+          isDeleted: false,
+          type: EProcurementType.FOURNITURES,
+        }),
+        this.dataServices.procurement_plans.count({
+          isDeleted: false,
+          type: EProcurementType.TRAVAUX,
+        }),
+        this.dataServices.procurement_plans.count({
+          isDeleted: false,
+          type: EProcurementType.PRESTATIONS_INTELLECTUELLES,
+        }),
+        this.dataServices.procurement_plans.count({
+          isDeleted: false,
+          type: EProcurementType.SERVICES_COURANTS,
+        }),
       ]);
       return succeed({
         code: HttpStatus.OK,
@@ -25,11 +57,18 @@ export class AppService {
           provisional_notice_awards,
           definitive_notice_awards,
           general_notices,
-        }
+          procurement_plans_fournitures,
+          procurement_plans_travaux,
+          procurement_plans_prestations,
+          procurement_plans_services,
+        },
       });
     } catch (error) {
       console.log({ error });
-      throw new HttpException(`Error while getting recap data. Try again.`, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        `Error while getting recap data. Try again.`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
